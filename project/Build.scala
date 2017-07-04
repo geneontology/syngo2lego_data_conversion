6004460567ea3f92884845f44c8888f90afe01c7
David Osumi-Sutherland <dosumis@gmail.com>
2017-07-04 18:03:44 +0100

    reverting master to old spec after accidentally updating

--- project/Build.scala
+++
@@ -1,8 +0,0 @@
-import sbt._
-
-
-object MyBuild extends Build 
-  { lazy val root = Project ( "root" , file ( "." )) dependsOn 
-                  ( syngo2lego ) 
-                  lazy val brainscowl = RootProject (
-                      uri ( "git://github.com/geneontology/syngo2lego.git)) }
